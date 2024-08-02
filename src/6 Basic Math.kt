872342685e5c25ebb2f20a60817697a854cdc19e
hamilbra002 <hamilbra002@warren.k12.in.us>
2024-08-02 12:02:22 -0400

    I did 1 - 7 problems

--- src/6 Basic Math.kt
+++ src/6 Basic Math.kt
@@ -6,9 +6,20 @@ fun main() {
     val result = add(firstNumber, secondNumber)
     val anotherResult = add(firstNumber, thirdNumber)
 
+    val subResult = subtract(firstNumber, secondNumber)
+    val subAnotherResult = subtract(firstNumber, thirdNumber)
+
     println("$firstNumber + $secondNumber = $result")
     println("$firstNumber + $thirdNumber = $anotherResult")
+
+    println("$firstNumber - $secondNumber = $subResult")
+    println("$firstNumber - $thirdNumber = $subAnotherResult")
 }
-fun add(Number1 : Int, Number2: Int): Int {
+
+fun add(Number1: Int, Number2: Int): Int {
     return Number1 + Number2;
+}
+
+fun subtract(number1 : Int, number2 : Int): Int{
+    return number1 - number2;
 }
